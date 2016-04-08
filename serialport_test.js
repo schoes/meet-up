d3c432a5af6931b3f2ba164d933c0f50b5bf5f4c
Martin Fischer <tinu.fischer@gmail.com>
2016-04-08 18:06:28 +0200

    add test with serial port (as alternative to johnny five api...)

---
+++ serialport_test.js
@@ -0,0 +1,20 @@
+/*
+ * Test with SerialPort...
+ * Notes:
+ * - npm install serialport
+ * - check correct port name
+ */
+var serialport = require('serialport');
+var portName = '/dev/cu.usbmodem1411';
+var sp = new serialport.SerialPort(portName, {
+    baudRate: 9600,
+    dataBits: 8,
+    parity: 'none',
+    stopBits: 1,
+    flowControl: false,
+    parser: serialport.parsers.readline("\r\n")
+});
+
+sp.on('data', function(input) {
+    console.log(input);
+});
