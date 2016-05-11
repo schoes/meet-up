a76cedac76147195da03f2e7f480c6438a1a905f
Martin Fischer <tinu.fischer@gmail.com>
2016-05-11 15:55:03 +0200

    make it runnable for two usb ports

--- serialread/boardReader.js
+++ serialread/boardReader.js
@@ -1,11 +1,18 @@
 var ws = require("nodejs-websocket")
+var serialport = require('serialport');
 
 // Global map which holds all desk states (with desk id as key)
 var deskStates = {};
 
-var serialport = require('serialport');
-var portName = '/dev/cu.usbmodem1411';
-var sp = new serialport.SerialPort(portName, {
+//       _______. _______ .______       __       ___       __         .______     ______   .______     .___________.
+//      /       ||   ____||   _  \     |  |     /   \     |  |        |   _  \   /  __  \  |   _  \    |           |
+//     |   (----`|  |__   |  |_)  |    |  |    /  ^  \    |  |        |  |_)  | |  |  |  | |  |_)  |   `---|  |----`
+//      \   \    |   __|  |      /     |  |   /  /_\  \   |  |        |   ___/  |  |  |  | |      /        |  |
+//  .----)   |   |  |____ |  |\  \----.|  |  /  _____  \  |  `----.   |  |      |  `--'  | |  |\  \----.   |  |
+//  |_______/    |_______|| _| `._____||__| /__/     \__\ |_______|   | _|       \______/  | _| `._____|   |__|
+//
+var portName1411 = '/dev/cu.usbmodem1411';
+var sp1411 = new serialport.SerialPort(portName1411, {
     baudRate: 9600,
     dataBits: 8,
     parity: 'none',
@@ -14,29 +21,17 @@ var sp = new serialport.SerialPort(portName, {
     parser: serialport.parsers.readline("\r\n")
 });
 
-// Scream server example: "hi" -> "HI!!!"
-//var server = ws.createServer();
-var websocketConnection = undefined;
+var portName1421 = '/dev/cu.usbmodem1421';
+var sp1421 = new serialport.SerialPort(portName1421, {
+    baudRate: 9600,
+    dataBits: 8,
+    parity: 'none',
+    stopBits: 1,
+    flowControl: false,
+    parser: serialport.parsers.readline("\r\n")
+});
 
-var server = ws.createServer(function (conn) {
-    console.log("New connection")
-    websocketConnection = conn;
-    conn.on("text", function (str) {
-        console.log("Received "+str)
-        conn.sendText(str.toUpperCase()+"!!!")
-    })
-    conn.on("close", function (code, reason) {
-        console.log("Connection closed")
-    })
-}).listen(8001)
-
-
-server.on("listening", function () {
-    console.log("Socket Ready (localhost:8001) ...")
-})
-
-console.log('Listening on Serial Port...');
-sp.on('data', function(input) {
+var readBoardInfo = function(input) {
 
     // parse ARD_STATE string
     if (input.startsWith("ARD_STATE")) {
@@ -51,7 +46,7 @@ sp.on('data', function(input) {
         var userTokens = userHex.split(" ");
         var user = '';
         for (i = 0; i < 7; i++) {
-           user += hex2a(userTokens[i]);
+            user += hex2a(userTokens[i]);
         }
 
         console.log('Processing Arduino state change...');
@@ -72,6 +67,17 @@ sp.on('data', function(input) {
 
         console.log("Desk States Map", deskStates);
     }
+}
+
+
+sp1411.on('data', readBoardInfo);
+sp1411.on('open', function() {
+    console.log('Listening on Serial Port 1411...');
+});
+
+sp1421.on('data', readBoardInfo);
+sp1421.on('open', function() {
+    console.log('Listening on Serial Port 1421...');
 });
 
 // helper function: convert hex to ascii
@@ -82,3 +88,29 @@ function hex2a(hexx) {
         str += String.fromCharCode(parseInt(hex.substr(i, 2), 16));
     return str;
 }
+
+//  ____    __    ____  _______ .______        _______.  ______     ______  __  ___  _______ .___________.
+//  \   \  /  \  /   / |   ____||   _  \      /       | /  __  \   /      ||  |/  / |   ____||           |
+//   \   \/    \/   /  |  |__   |  |_)  |    |   (----`|  |  |  | |  ,----'|  '  /  |  |__   `---|  |----`
+//    \            /   |   __|  |   _  <      \   \    |  |  |  | |  |     |    <   |   __|      |  |
+//     \    /\    /    |  |____ |  |_)  | .----)   |   |  `--'  | |  `----.|  .  \  |  |____     |  |
+//      \__/  \__/     |_______||______/  |_______/     \______/   \______||__|\__\ |_______|    |__|
+//
+var websocketConnection = undefined;
+
+var server = ws.createServer(function (conn) {
+    console.log("New connection")
+    websocketConnection = conn;
+    conn.on("text", function (str) {
+        console.log("Received "+str)
+        conn.sendText(str.toUpperCase()+"!!!")
+    })
+    conn.on("close", function (code, reason) {
+        console.log("Connection closed")
+    })
+}).listen(8001)
+
+
+server.on("listening", function () {
+    console.log("Socket Ready (localhost:8001) ...")
+})
